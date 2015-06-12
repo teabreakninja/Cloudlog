9843e36867d20ecb2e975fee09acf99cd01dbc75
Peter Goodhall <peter@magicbug.co.uk>
2015-06-12 19:25:28 +0100

    removed old files

--- application/views/adif/import_success.php
+++
@@ -1,6 +0,0 @@
-<div id="container">
-<h2>ADIF Imported</h2>
-
-<p>The ADIF File has been imported, and any dupes skipped.</p>
-
-</div>
--- application/views/lotw/analysis.php
+++
@@ -1,6 +0,0 @@
-<div id="container">
-<h2><?php echo $page_title; ?></h2>
-
-<?php echo $lotw_table; ?>
-
-</div>
